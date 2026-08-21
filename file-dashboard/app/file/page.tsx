6a8742c4c0e08209942e636b7e780d52b6b1b87c
jungwook <mr4454@naver.com>
2026-08-22 04:10:16 +0900

    design: upload component centering

--- file-dashboard/app/file/page.tsx
+++ file-dashboard/app/file/page.tsx
@@ -5,7 +5,15 @@ import UploadFileBox from "@/components/UploadFileBox";
 
 export default function FilePage() {
   return (
-    <div style={{ padding: 40, display: "flex", justifyContent: "center" }}>
+    <div
+      style={{
+        padding: 40,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "100vh",
+      }}
+    >
       <UploadFileBox
         onUploadSuccess={() => console.log("Refresh the file list!")}
       />
