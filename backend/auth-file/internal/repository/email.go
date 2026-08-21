ea6ef1c9b6ee2688628ae603a5f9c1d7d8aa1249
jungwook <mr4454@naver.com>
2026-08-21 22:00:46 +0900

    chore: FindLoginInfoByEmail이 실패했을 때 디버그용 로깅추가

--- backend/auth-file/internal/repository/email.go
+++ backend/auth-file/internal/repository/email.go
@@ -2,11 +2,15 @@ package repository
 
 import (
 	"context"
+	"log/slog"
 
 	"github.com/google/uuid"
 )
 
 func (r *Repository) FindLoginInfoByEmail(ctx context.Context, email string) (id uuid.UUID, password string, role string, err error) {
 	err = r.pool.QueryRow(ctx, `SELECT id, password, role FROM member WHERE email = $1`, email).Scan(&id, &password, &role)
+	if err != nil {
+		slog.Error("fail to find login info by email", "err", err)
+	}
 	return id, password, role, err
 }
